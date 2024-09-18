07d68aa7b0b4bf2dbb6587b45b6ce6f3dc0fe46b
LiCai <1981824361@qq.com>
2024-09-18 09:10:42 +0800

    feat: Nodejs

--- src/npmDown.js
+++ src/npmDown.js
@@ -1,4 +1,6 @@
 import puppeteer from "puppeteer";
+import { fsWrite } from "./util/index.js";
+import { fsMd } from "./util/index.js";
 // Or import puppeteer from 'puppeteer-core';
 
 const test = async () => {
@@ -7,55 +9,45 @@ const test = async () => {
     headless: true,
     args: ["--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage"],
   });
-  // browser.on("targetcreated", async (target) => {
-  //   if (target.type() === "page") {
-  //     const newPage = target.page();
-  //     if (newPage) {
-  //       // 检查新页面的 URL 或其他特征以确定是否是目标新标签页
-  //       // if (newPage.url().includes('/target-page')) {
-  //       //   newPage.waitForNavigation({ waitUntil: 'networkidle2' }).then(() => {
-  //       //     // 在新标签页进行抓取操作
-  //       //     const title = newPage.title();
-  //       //   });
-  //       // }
-  //       const title = await newPage.$(".hot-list-header .hot-title");
-  //       console.log(title);
-  //       await browser.close();
-  //     }
-  //   }
-  // });
   const page = await browser.newPage();
 
   // Navigate the page to a URL.
-  await page.goto("https://juejin.cn/", {
+  await page.goto("https://nodejs.cn/", {
     waitUntil: "load",
     timeout: 60000,
   });
 
-  console.log(page.url());
+  await page.waitForSelector('[class="cate_item"]');
+  const result = await page.evaluate(() => {
+    const titleList = document.querySelectorAll(
+      '#partner_item_box [class="cate_item"] [class="partner_item"] [class="partner_name"]'
+    );
+    let title = [];
+    titleList.forEach((element) => {
+      title.push(element.innerText);
+    });
+    return title;
+  });
 
-  await page.waitForSelector(
-    ".side-navigator-wrap .nav-item-wrap:nth-child(12) .nav-item-content a"
+  const hrefList = await page.$$eval(
+    '#partner_item_box [class="cate_item"] [class="partner_item"]',
+    (elements) => {
+      return elements.map((element) => {
+        return element.href;
+      });
+    }
   );
-  await page.$eval(
-    ".side-navigator-wrap .nav-item-wrap:nth-child(12) .nav-item-content a",
-    (node) => node.click()
-  );
 
-  const newPagePromise = new Promise((x) =>
-    browser.once("targetcreated", (target) => x(target.page()))
-  ); // 声明变量
+  const finalResult = result.map((item, index) => {
+    return {
+      title: item,
+      url: hrefList[index],
+    };
+  });
 
-  let newPage = await newPagePromise; // newPage就是a链接打开窗口的Page对象
+  const title = await page.title();
 
-  console.log(newPage.url());
-  await newPage.waitForTimeout(1000 * 2);
-  // const title = await newPage.$(".hot-list-header .hot-title");
-  const text = await newPage.$eval(
-    ".hot-list-header .hot-title",
-    (element) => element.innerText
-  );
-  console.log(text);
+  await fsWrite("nodejs", fsMd(finalResult, title), "md");
   await browser.close();
 };
 
